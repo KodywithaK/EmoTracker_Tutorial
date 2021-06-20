34b34fc2aee03e53f18bf0c23be303d731c7d1a5
s221270 <s221270@nwytg.net>
2021-06-20 00:08:03 -0700

    first commit

---
+++ EmoTracker/packs/Tutorial_KodywithaK/scripts/init.lua
@@ -0,0 +1,10 @@
+--  Load configuration options up front
+ScriptHost:LoadScript("scripts/settings.lua")
+
+Tracker:AddItems("items/common.json")
+Tracker:AddItems("items/dungeon_items.json")
+Tracker:AddItems("items/keys.json")
+Tracker:AddItems("items/labels.json")
+Tracker:AddMaps("maps/maps.json")
+Tracker:AddLayouts("layouts/items.json")
+Tracker:AddLayouts("layouts/tracker.json")
