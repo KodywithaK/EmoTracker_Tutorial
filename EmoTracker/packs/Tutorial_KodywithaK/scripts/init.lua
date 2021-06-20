4cd0f77ad18d7d3edc0dc737aa24759dbc7cdb72
s221270 <s221270@nwytg.net>
2021-06-20 00:40:05 -0700

    Test_Map

--- EmoTracker/packs/Tutorial_KodywithaK/scripts/init.lua
+++ EmoTracker/packs/Tutorial_KodywithaK/scripts/init.lua
@@ -2,9 +2,9 @@
 ScriptHost:LoadScript("scripts/settings.lua")
 
 Tracker:AddItems("items/common.json")
-Tracker:AddItems("items/dungeon_items.json")
-Tracker:AddItems("items/keys.json")
-Tracker:AddItems("items/labels.json")
+--Tracker:AddItems("items/dungeon_items.json")
+--Tracker:AddItems("items/keys.json")
+--Tracker:AddItems("items/labels.json")
 Tracker:AddMaps("maps/maps.json")
 Tracker:AddLayouts("layouts/items.json")
 Tracker:AddLayouts("layouts/tracker.json")
